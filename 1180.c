34be98c35e7851bd51fd4dbb1799c932d2570ef8
Muralha <viccandido367@gmail.com>
2026-08-30 11:34:36 -0300

    Update 1180.c

--- 1180.c
+++ 1180.c
@@ -2,21 +2,17 @@
 
 int main(){
     //Verify interval
-    int N = 0, pos = 0;
-    while(N < 1 || N > 1000){
-        scanf("%d", &N);
-    }
-    //Creating X
+    int N = 100, pos = 0;
     int X[N];
     for(int i = 0; i < N; i++){
         scanf("%d", &X[i]);
     }
     //Searching the smallest one
     for(int i = 0; i < N; i++){
-       if(X[pos] >= X[i]) pos = i;
+       if(X[pos] <= X[i]) pos = i;
     }
     //Printing the result
-    printf("Menor valor: %d\n", X[pos]);
-    printf("Posicao: %d\n", pos);
+    printf("%d\n", X[pos]);
+    printf("%d\n", pos+1);
     return 0;
 }
