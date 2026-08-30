86896e52405652dd4b111c037dea9121ef5eab0f
Muralha <viccandido367@gmail.com>
2026-08-30 11:45:47 -0300

    Add files via upload

---
+++ 1080b.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    //Verify interval
+    int N = 100, pos = 0;
+    int *X = (int*)malloc(N*sizeof(int));
+    
+    //Creating X
+    for(int i = 0; i < N; i++){
+        scanf("%d", &X[i]);
+    }
+
+    //Searching the smallest one
+    for(int i = 0; i < N; i++){
+       if(X[pos] < X[i]) pos = i;
+    }
+
+    //Printing the result
+    printf("%d\n", X[pos]);
+    printf("%d\n", pos+1);
+    free(X);
+    return 0;
+}
